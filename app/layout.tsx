6c53d64e2f49277be08cb9f52cf64c759b5750f9
Sava Stanchev <s.stanchev94@gmail.com>
2023-08-09 23:01:30 +0300

    init with create next app

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "lil messenger",
+  description: "it's a little messenger",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
